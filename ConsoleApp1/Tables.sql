89bf50d4ee8827e3266c96654c854e349bc134db
caguirre97 <caguirre97@OBAMA2>
2018-11-07 21:42:31 -0600

    added reviews, libraries and users initial table creation

--- ConsoleApp1/Tables.sql
+++ ConsoleApp1/Tables.sql
@@ -3,8 +3,24 @@ GO
 drop table if exists gitSteamed.Items
 GO
 drop table if exists gitSteamed.Bundles
-go
+GO
+DROP TABLE IF EXISTS gitSteamed.Reviews;
+GO
+DROP TABLE IF EXISTS gitSteamed.Users;
+GO
+DROP TABLE IF EXISTS gitSteamed.Libraries;
+GO
 
+CREATE TABLE gitSteamed.Users (
+	Username NVARCHAR(64) NOT NULL PRIMARY KEY,
+	ItemCount INT NOT NULL,
+	[Url] NVARCHAR(256) NOT NULL,
+	CONSTRAINT [UK_gitSteamed_Users_Url] UNIQUE
+	(
+		[Url] ASC
+	)
+)
+GO
 
 create table gitSteamed.BundleContents
 (
@@ -14,6 +30,7 @@ create table gitSteamed.BundleContents
 
 	unique(BundleID, ItemID)
 )
+GO
 
 create table gitSteamed.Items
 (
@@ -32,5 +49,42 @@ create table gitSteamed.Bundles
 	[URL] NVARCHAR(256) not null unique,
 	FinalPrice float not null,
 	DiscountPrice float null
+)
+GO
+
+
+CREATE TABLE gitSteamed.Reviews (
+	ReviewID INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	Username NVARCHAR(64) NOT NULL FOREIGN KEY
+								REFERENCES gitSteamed.Users(Username),
+	ItemId INT NOT NULL FOREIGN KEY
+								REFERENCES gitSteamed.Items(ItemID),
+	Funny INT NOT NULL DEFAULT(0),
+	Posted DATETIMEOFFSET NOT NULL DEFAULT(SYSDATETIMEOFFSET()),
+	LastEdited DATETIMEOFFSET NOT NULL DEFAULT(SYSDATETIMEOFFSET()),
+	Helpful INT NULL,
+	Recommend BIT NOT NULL,
+	[Description] NVARCHAR(512) NOT NULL DEFAULT(N'No text'),
+	CONSTRAINT [UK_gitSteamed_Library_Username_TermId] UNIQUE
+	(
+		Username ASC,
+		ItemId ASC
+	)
+)
+GO
+
+CREATE TABLE gitSteamed.Libraries (
+	LibraryID INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	Username NVARCHAR(64) NOT NULL FOREIGN KEY
+								REFERENCES gitSteamed.Users(Username),
+	ItemId INT NOT NULL FOREIGN KEY
+								REFERENCES gitSteamed.Items(ItemID),
+	TimePlayedForever INT NOT NULL DEFAULT(0),
+	TimePlayed2Weeks INT NOT NULL DEFAULT(0),
+	CONSTRAINT [UK_gitSteamed_Library_Username_TermId] UNIQUE
+	(
+		Username ASC,
+		ItemId ASC
+	)
 )
 GO
