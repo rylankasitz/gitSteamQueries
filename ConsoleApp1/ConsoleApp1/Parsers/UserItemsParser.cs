15a5d087fdfa17ef0815051c64378789426f4c38
Rylan Kasitz <rylankasitz@ksu.edu>
2018-11-15 01:09:26 -0800

    pushing to remote in

--- ConsoleApp1/ConsoleApp1/Parsers/UserItemsParser.cs
+++ ConsoleApp1/ConsoleApp1/Parsers/UserItemsParser.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Text;
+using System.Transactions;
 using GitSteamedDatabase.JsonObjects;
+using System.Data;
 
 namespace GitSteamedDatabase.Parsers
 {
@@ -13,7 +16,35 @@ namespace GitSteamedDatabase.Parsers
 
         public override void Parse()
         {
-            
+            foreach (UserItem userItem in DataManager.UserItems)
+            {
+                _AddUser(userItem.user_id, userItem.user_url, userItem.item_count);
+                foreach (Item item in userItem.items)
+                {
+
+                }
+            }
+        }
+
+        private void _AddUser(string userName, string userURL, int itemCount)
+        {
+            using (var transaction = new TransactionScope())
+            {
+                using (var connection = new SqlConnection(DataManager.Connection))
+                {
+                    using (var command = new SqlCommand("Demo.AddUser", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.Parameters.AddWithValue("username", userName);
+                        command.Parameters.AddWithValue("url", userURL);
+                        command.Parameters.AddWithValue("itemcount", itemCount);
+
+                        connection.Open();
+                        command.ExecuteNonQuery();
+                        transaction.Complete();
+                    }
+                }
+            }
         }
     }
 }
