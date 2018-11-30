0c420f662587456e095094b47f96c90493a93ad0
caguirre97 <caguirre97@OBAMA2>
2018-11-30 10:39:44 -0600

    small changes to add library procedure

--- ConsoleApp1/ConsoleApp1/Queries/Procedures/AddToDatabase/AddToLibrary.sql
+++ ConsoleApp1/ConsoleApp1/Queries/Procedures/AddToDatabase/AddToLibrary.sql
@@ -1,1 +1,17 @@
-﻿
+﻿CREATE OR ALTER PROCEDURE gitSteamed.AddLibrary
+	@Username NVARCHAR(64),
+	@ItemId INT,
+	@TimePlayedForever INT,
+	@TimePlayed2Weeks INT
+AS
+
+WITH LibraryCTE(Username, ItemId, TimePlayedForever, TimePlayed2Weeks) AS (
+	SELECT *
+	FROM (VALUES(@Username, @ItemId, @TimePlayedForever, @TimePlayed2Weeks)) AS P(Username, ItemId, TimePlayedForever, TimePlayed2Weeks)
+)
+MERGE gitSteamed.Library L
+USING LibraryCTE C ON C.Username = L.Username AND C.ItemId = L.ItemId
+WHEN NOT MATCHED THEN
+	INSERT (Username, ItemId, TimePlayedForever, TimePlayed2Weeks)
+	VALUES(@Username, @ItemId, @TimePlayedForever, @TimePlayed2Weeks);
+GO
